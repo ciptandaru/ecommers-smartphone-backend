399bb9d59b736531ca4578c141612f26369f319d
ciptandaru <ciptandaru@gmail.com>
2024-05-01 21:56:38 +0700

    unit test code

--- __test__/product.test.js
+++ __test__/product.test.js
@@ -4,6 +4,203 @@ const {Sequelize} = require("../models");
 const {generateToken} = require("../helper/jwt");
 jest.setTimeout(2000);
 
+describe("Admin Controller Test", () => {
+  test("Register Admin Success", async () => {
+    const fakeAccessToken = generateToken({
+      id: 1,
+      userName: "TestAdmin",
+      admin: true,
+    });
+
+    const response = await request(app)
+      .post("/admin/register")
+      .set("access_token", fakeAccessToken)
+      .send({
+        userName: "TestAdmin",
+        email: "testadmin@example.com",
+        password: "123123",
+      });
+    expect(response.status).toBe(201);
+  });
+
+  test("Login Admin Success", async () => {
+    const response = await request(app).post("/admin/login").send({
+      email: "admin@gmail.com",
+      password: "admin",
+    });
+    expect(response.status).toBe(200);
+  });
+
+  test("Create Product Success", async () => {
+    const fakeAccessToken = generateToken({
+      id: 1,
+      userName: "TestAdmin",
+      admin: true,
+    });
+
+    const response = await request(app)
+      .post("/admin/product")
+      .set("access_token", fakeAccessToken)
+      .send({
+        name: "Test Product",
+        description: "This is a test product",
+        price: 100,
+        image: "test.jpg",
+        CategoryId: 1,
+      });
+    expect(response.status).toBe(201);
+  });
+
+  test("get all Product Success", async () => {
+    const fakeAccessToken = generateToken({
+      id: 1,
+      userName: "TestAdmin",
+      admin: true,
+    });
+
+    const response = await request(app)
+      .get("/admin/product")
+      .set("access_token", fakeAccessToken);
+    expect(response.status).toBe(200);
+  });
+
+  test("get  Product Success", async () => {
+    const fakeAccessToken = generateToken({
+      id: 1,
+      userName: "TestAdmin",
+      admin: true,
+    });
+
+    const response = await request(app)
+      .get("/admin/product/?page=1&pageLimit=5")
+      .set("access_token", fakeAccessToken);
+    expect(response.status).toBe(200);
+  });
+
+  test("get different page Product Success", async () => {
+    const fakeAccessToken = generateToken({
+      id: 1,
+      userName: "TestAdmin",
+      admin: true,
+    });
+
+    const response = await request(app)
+      .get("/admin/product/?page=2&pageLimit=5")
+      .set("access_token", fakeAccessToken);
+    expect(response.status).toBe(200);
+  });
+
+  test("get filtered and  Product Success", async () => {
+    const fakeAccessToken = generateToken({
+      id: 1,
+      userName: "TestAdmin",
+      admin: true,
+    });
+
+    const response = await request(app)
+      .get("/admin/product/?page=1&pageLimit=5&search=iphone")
+      .set("access_token", fakeAccessToken);
+    expect(response.status).toBe(200);
+  });
+
+  test("get one Product Success", async () => {
+    const fakeAccessToken = generateToken({
+      id: 1,
+      userName: "TestAdmin",
+      admin: true,
+    });
+
+    const id = 5;
+    const response = await request(app)
+      .get(`/admin/product/${id}`)
+      .set("access_token", fakeAccessToken);
+    expect(response.status).toBe(200);
+  });
+
+  test("get one Product failed", async () => {
+    const fakeAccessToken = generateToken({
+      id: 1,
+      userName: "TestAdmin",
+      admin: true,
+    });
+
+    const id = 40;
+    const response = await request(app)
+      .get(`/admin/product/${id}`)
+      .set("access_token", fakeAccessToken);
+    expect(response.status).toBe(404);
+  });
+
+  test("Update Product Success", async () => {
+    const fakeAccessToken = generateToken({
+      id: 1,
+      userName: "TestAdmin",
+      admin: true,
+    });
+
+    const id = 5;
+    const response = await request(app)
+      .patch(`/admin/product/${id}`)
+      .set("access_token", fakeAccessToken)
+      .send({
+        name: "Update Product",
+        description: "This is a Update product",
+        price: 100,
+        image: "tesUpdate.jpg",
+        CategoryId: 1,
+      });
+    expect(response.status).toBe(201);
+  });
+
+  test("Update Product Failed Authorized", async () => {
+    const fakeAccessToken = generateToken({
+      id: 100,
+      userName: "nonAdmin",
+      admin: false,
+    });
+
+    const id = 1;
+    const response = await request(app)
+      .delete(`/admin/product/${id}`)
+      .set("access_token", fakeAccessToken);
+    expect(response.status).toBe(401);
+  });
+  test("Delete Product Success", async () => {
+    const fakeAccessToken = generateToken({
+      id: 1,
+      userName: "TestAdmin",
+      admin: true,
+    });
+
+    const id = 24;
+    const response = await request(app)
+      .delete(`/admin/product/${id}`)
+      .set("access_token", fakeAccessToken);
+    expect(response.status).toBe(200);
+  });
+
+  test("Delete Product Failed Authorized", async () => {
+    const fakeAccessToken = generateToken({
+      id: 100,
+      userName: "nonAdmin",
+      admin: false,
+    });
+
+    const id = 25;
+    const response = await request(app)
+      .patch(`/admin/product/${id}`)
+      .set("access_token", fakeAccessToken)
+      .send({
+        name: "Update Product",
+        description: "This is a Update product",
+        price: 100,
+        image: "tesUpdate.jpg",
+        CategoryId: 1,
+      });
+    expect(response.status).toBe(401);
+  });
+});
+
 describe("Customer Controller Test", () => {
   test("register success", async () => {
     const response = await request(app).post("/register").send({
@@ -115,7 +312,7 @@ describe("Customer Controller Test", () => {
   });
 
   test("get one Product Success", async () => {
-    const id = 1;
+    const id = 5;
     const response = await request(app).get(`/product/${id}`);
     expect(response.status).toBe(200);
   });
@@ -124,53 +321,5 @@ describe("Customer Controller Test", () => {
     const id = 40;
     const response = await request(app).get(`/product/${id}`);
     expect(response.status).toBe(404);
-  });
-});
-
-describe("Admin Controller Test", () => {
-  test("Login Admin Success", async () => {
-    const response = await request(app).post("/admin/login").send({
-      email: "admin@gmail.com",
-      password: "admin",
-    });
-    expect(response.status).toBe(200);
-  });
-
-  test("Register Admin Success", async () => {
-    const fakeAccessToken = generateToken({
-      id: 1,
-      userName: "TestAdmin",
-      admin: true,
-    });
-
-    const response = await request(app)
-      .post("/admin/register")
-      .set("access_token", fakeAccessToken)
-      .send({
-        userName: "TestAdmin",
-        email: "testadmin@example.com",
-        password: "123123",
-      });
-    expect(response.status).toBe(201);
-  });
-
-  test("Create Product Success", async () => {
-    const fakeAccessToken = generateToken({
-      id: 1,
-      userName: "TestAdmin",
-      admin: true,
-    });
-
-    const response = await request(app)
-      .post("/admin/product")
-      .set("access_token", fakeAccessToken)
-      .send({
-        name: "Test Product",
-        description: "This is a test product",
-        price: 100,
-        image: "test.jpg",
-        CategoryId: 1,
-      });
-    expect(response.status).toBe(201);
   });
 });
